476c12041e99cdeb4feeafca33159bdb05d6eebc
Alekhyo Banerjee <59485605+Alekhyo@users.noreply.github.com>
2020-03-28 10:18:19 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,22 @@
+#DSA-Assgn-1
+
+def merge_list(list1, list2):
+    merged_data=""
+    for i in range(0,max(len(list1),len(list2))):
+        if list1[i]==None:
+            merged_data=merged_data+' '+list2[-(i+1)]
+        elif list2[-(i+1)]==None:
+            merged_data=merged_data+' '+list1[i]
+        else:
+            merged_data=merged_data+' '+list1[i]+list2[-(i+1)]
+    resultant_data=merged_data[1:]
+    return resultant_data
+
+
+
+
+#Provide different values for the variables and test your program
+list1=['A', 'app','a', 'd', 'ke', 'th', 'doc', 'awa']
+list2=['y','tor','e','eps','ay',None,'le','n']
+merged_data=merge_list(list1,list2)
+print(merged_data)
